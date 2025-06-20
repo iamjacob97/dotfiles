c5190b40a6d50588425492254b777f539681f74e
iamjacob97 <jacobabraham1997@gmail.com>
2025-06-20 23:44:15 +0100

    Changed themes

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,29 +1,27 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.initial_cols = 121
-config.initial_rows = 27
+config.initial_cols = 139
+config.initial_rows = 33
 
-config.color_scheme = "nightfox"
+config.color_scheme = "Kanagawa (Gogh)"
 config.font_size = 11
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 config.window_padding = {
-  left = '0.9cell',
-  right = '0.9cell',
-  top = '0.5cell',
-  bottom = '0.5cell',
+	left = "0.9cell",
+	right = "0.9cell",
+	top = "0.3cell",
+	bottom = "0.3cell",
 }
 config.window_background_opacity = 0.97
 config.text_background_opacity = 0.97
-
-config.default_domain = "WSL:FedoraLinux-42"
 
 return config
